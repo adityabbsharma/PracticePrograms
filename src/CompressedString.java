6cf2e60134b9241de3fcea99834b80e79b2080bb
adityabbsharma <adityabbsharma@gmail.com>
2021-07-19 22:07:06 +0530

    Compressed String program added

--- src/CompressedString.java
+++ src/CompressedString.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 * Problem Description
 You are given a string S you have to implement a function to perform basic string compression
@@ -27,5 +29,40 @@ First 'a' repeats two times then 'b' doesn't repeats then 'c' repeats two times.
 
 Constraints
 1 <= length(S) <= 100000
-* */public class CompressedString {
+* */
+public class CompressedString {
+    public static String compress(String s) {
+        StringBuffer str = new StringBuffer(s);
+        StringBuffer finalStr = new StringBuffer();
+        int i=0;
+        Scanner scanner = new Scanner(System.in);
+        while(i<str.length()){
+            int j =i;
+            int count=1;
+            if(j+1 < str.length()){
+                while(str.charAt(j)==str.charAt(j+1)){
+                    count++;
+                    j++;
+                    if(j+1==str.length()){
+                        break;
+                    }
+                }
+            }
+            finalStr.append(str.charAt(i));
+            finalStr.append(count);
+            i=j+1;
+        }
+        if(finalStr.length()>=str.length()){
+            return str.toString();
+        }
+        return finalStr.toString();
+    }
+    public static void  main (String args []) {
+        Scanner sc =  new Scanner(System.in);
+        String s = new String();
+        s = sc.next();
+        int n = s.length();
+        String ans = compress(s);
+        System.out.print(ans);
+    }
 }
